bf1f6021151aa7eb94d39b866eb5c791a1e4efb0
Dan Ohlin <dan.ohlin@outlook.com>
2016-10-02 20:36:40 +0000

    create initial structure

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,34 @@
 class Enigma
+  
   def self.on
+    puts "class - I'm on"
+  end
+  
+  def initialize(names = "World")
+    @names = names
+  end
+  
+  def encrypt(text)
+    text.swapcase
+  end
+  
+
+  
+  def on
     puts "I'm on"
   end
+  
+  
+  if __FILE__ == $0
+    
+    machine = Enigma.new
+    #puts machine.methods
+    #machine.on
+    
+    puts "Enter letter to encrypt:"
+    plaintext = STDIN.gets.chomp
+    puts machine.encrypt(plaintext)
+  
+  end
+  
 end
